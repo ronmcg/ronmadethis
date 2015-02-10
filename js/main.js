3a7c3f699607e23b6d123316ad27b6a7a371e6a9
Ronnie McGregor <rmcgr467@mtroyal.ca>
2015-02-10 13:59:08 -0700

    added more content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-
+//Handle clicks on the circles
+$('#b1').click(function(e) {
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: $('#section1').offset().top
+  }, 1000);
+});
+$('#b2').click(function(e) {
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: $('#section2').offset().top
+  }, 1000);
+});
+$('#b3').click(function(e) {
+  e.preventDefault();
+  $('html, body').animate({
+    scrollTop: $('#section3').offset().top
+  }, 1000);
+});
